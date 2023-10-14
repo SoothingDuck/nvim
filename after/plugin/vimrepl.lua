ad0bee79cb2865334e3bab047d65725a8d8a94d7
Orpheus <orpheus@mampang.org>
2023-10-14 15:38:49 +0200

    trop top

--- after/plugin/vimrepl.lua
+++ after/plugin/vimrepl.lua
@@ -8,3 +8,5 @@ vim.g["repl_split"] = "bottom"
 
 vim.keymap.set("n", "<leader>rt", vim.cmd.ReplToggle)
 vim.keymap.set("n", "<leader>rc", vim.cmd.ReplRunCell)
+vim.keymap.set("n", "<leader>rs", vim.cmd.ReplSend)
+vim.keymap.set("v", "<leader>rs", ":ReplSendVisual<CR>")
