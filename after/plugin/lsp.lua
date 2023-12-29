2bd631c3bbaa36b0b62e926fc2c6b808c7a6fe42
Orpheus <orpheus@mampang.org>
2023-12-29 21:32:11 +0100

    yip

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls', 'eslint', 'pyright'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls', 'eslint', 'pyright', 'r_language_server', 'hls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
